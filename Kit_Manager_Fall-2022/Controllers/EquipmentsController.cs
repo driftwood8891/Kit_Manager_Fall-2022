c7a8f4443a5ae9b9dac1cc19d237992335cee862
Oren Kirchhoff <133886@mail.macc.edu>
2022-11-16 23:56:57 -0600

    Created validation for Student management and built the Students Controller.  Everything is tested and is working.

---
+++ Kit_Manager_Fall-2022/Controllers/EquipmentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Kit_Manager_Fall_2022.Data;
+using Kit_Manager_Fall_2022.Models;
+
+namespace Kit_Manager_Fall_2022.Controllers
+{
+    public class EquipmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipments
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Equipment.Include(e => e.Student);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Equipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment
+                .Include(e => e.Student)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public IActionResult Create()
+        {
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId");
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ItemId,ItemType,ItemName,ItemCost,KitId,StatusCode,StudentId,LastEdit,InstructorName,Course,Active")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", equipment.StudentId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", equipment.StudentId);
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ItemId,ItemType,ItemName,ItemCost,KitId,StatusCode,StudentId,LastEdit,InstructorName,Course,Active")] Equipment equipment)
+        {
+            if (id != equipment.ItemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.ItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", equipment.StudentId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment
+                .Include(e => e.Student)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipment = await _context.Equipment.FindAsync(id);
+            _context.Equipment.Remove(equipment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+            return _context.Equipment.Any(e => e.ItemId == id);
+        }
+    }
+}
--- Kit_Manager_Fall-2022/Models/Equipment.cs
+++ Kit_Manager_Fall-2022/Models/Equipment.cs
@@ -12,15 +12,36 @@ namespace Kit_Manager_Fall_2022.Models
     {
         [Key]
         public int ItemId { get; set; }
+        [Display(Name = "Item Type")]
+        [Required(ErrorMessage = "Type of item is required.")]
         public string ItemType { get; set; }
+        [Display(Name = "Item Name")]
+        [Required(ErrorMessage = "Item name is required.")]
         public string ItemName { get; set; }
+        [Display(Name = "Item Cost")]
+        [Required(ErrorMessage = "Item cost is required.")]
         public int ItemCost { get; set; }
+        [Display(Name = "Kit ID")]
+        [Required(ErrorMessage = "Kit ID is required.")]
         public int KitId { get; set; }
+        [Display(Name = "Status Code")]
+        [Required(ErrorMessage = "Enter a number between 1 and 9.")]
+        [Range (1,9)]
         public int StatusCode { get; set; }
+        [Display(Name = "Student ID")]
+        [Required(ErrorMessage = "Student ID is required.")]
         public string StudentId { get; set; }
+        [Display(Name = "Last Change or Edit")]
+        [Required(ErrorMessage = "Date and Time is required.")]
         public DateTime LastEdit { get; set; }
+        [Display(Name = "Instructor Name")]
+        [Required(ErrorMessage = "Instructor name is required.")]
         public string InstructorName { get; set; }
+        [Display(Name = "Course")]
+        [Required(ErrorMessage = "Name of course is required.")]
         public string Course { get; set; }
+        [Display(Name = "Is Item Active?")]
+        [Required(ErrorMessage = "'Y' or 'N' required.")]
         public string Active { get; set; }
 
         public virtual Status StatusCodeNavigation { get; set; }
