f53d00f979164d134510b9119fb9d5f3b4fbe185
drift <drift@DESKTOP-K2JPLS4>
2022-11-16 20:12:10 -0600

    Add project files.

---
+++ Kit_Manager_Fall-2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kit_Manager_Fall_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
