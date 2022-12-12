9bd778ba50109a9477cc558dd7a15c68cd1c8fe8
mikemikee21 <kariukimmk@gmail.com>
2022-12-11 19:42:31 -0600

    Comments added

--- Kit_Manager_Fall-2022/Models/Student.cs
+++ Kit_Manager_Fall-2022/Models/Student.cs
@@ -19,39 +19,50 @@ namespace Kit_Manager_Fall_2022.Models
         [Display(Name = "Student ID")]                        // Display field name
         [Required(ErrorMessage = "Student ID is required.")]  // Requires you to enter data into the field or will output error message
         public string StudentId { get; set; }
+
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "First name is required.")]
         public string FName { get; set; }
+
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Last name is required.")]
         public string LName { get; set; }
+
         [Display(Name = "College Email")]
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string CollegeEmail { get; set; }
+
         [Display(Name = "Personal Email")]
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string PersonalEmail { get; set; }
+
         [Display(Name = "Phone Number")]
         [Required(ErrorMessage = "Phone number is required.")]
         [Phone]
         public string Phone { get; set; }
+
         [Display(Name = "Address")]
         [Required(ErrorMessage = "Address is required.")]
         public string Address { get; set; }
+
         [Display(Name = "Campus")]
         [Required(ErrorMessage = "Name of Campus is required.")]
         public string Campus { get; set; }
+
         [Display(Name = "Is Student Active?")]
         [Required(ErrorMessage = "'Y' or 'N' is required.")]
         public string Active { get; set; }
+
         [Display(Name = "Does the Student have a kit?")]
         [Required(ErrorMessage = "'Y' or 'N' is required.")]
         public string HasItem { get; set; }
+
         [Display(Name = "Last Change or Edit")]
         [Required(ErrorMessage = "Date and time is required.")]
         public DateTime LastEdit { get; set; }
+
         [Display(Name = "Emergency Contact")]
         [Required(ErrorMessage = "Name, Phone #, and Address is required.")]
         public string EmergencyContact { get; set; }
